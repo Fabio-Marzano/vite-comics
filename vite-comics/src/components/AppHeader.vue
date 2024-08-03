244bd18e4854fbbd34ed1e37590a55452dc537aa
fabiomarzano <fabiomarzano1993@gmail.com>
2024-08-03 19:36:18 +0200

    [UPD] create-app-header

---
+++ vite-comics/src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            links: [
+                { text: 'Characters', url: '#', current: false },
+                { text: 'Comics', url: '#', current: true },
+                { text: 'Movies', url: '#', current: false },
+                { text: 'TV', url: '#', current: false },
+                { text: 'Games', url: '#', current: false },
+                { text: 'Collectibles', url: '#', current: false },
+                { text: 'Videos', url: '#', current: false },
+                { text: 'Fans', url: '#', current: false },
+                { text: 'News', url: '#', current: false },
+                { text: 'Shop', url: '#', current: false }
+            ]
+        }
+    },
+
+}
+
+</script>
+
+<template>
+
+    <header class="container">
+        <figure class="nav-logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+        <ul class="nav-links">
+            <li v-for="link in links" :key="link.text">
+                <a :href="link.url" :class="{ 'active': link.current }">{{ link.text }}</a>
+            </li>
+        </ul>
+    </header>
+
+
+</template>
+
+
